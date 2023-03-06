f04d187acecb6185e79f76a01f3ea22d34f35c0c
Xtreme-Flash <doorway125@gmail.com>
2023-03-06 22:24:44 +0530

    added collections

--- app.js
+++ app.js
@@ -1,54 +1,33 @@
+const mongoose = require('mongoose');
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB',{useNewUrlParser: true});
 
-//connection URL
-const url = 'mongodb://localhost:27017';
-
-//Database Name
-const dbName = 'fuitsDB';
-
-//Create a new MongoClient
-const client =  new MongoClient(url, { useNewUrlParser: true });
-
-// Use connect method to connect to the server
-client.connect(function(err){
-    assert.equal(null, err);
-    console.log("Connected succsessfully to server");
-
-    const db = client.db(dbName);
-
-    insertDocuments(db, function(){
-       client.close();
-    });
-   
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    rating: Number,
+    review: String
 });
 
-const insertDocuments = function(db, callback) {
-    // get the documents collection
-    const collection = db.collection('fruits');
-    // insert some documments
-    collection.insertMany([
-        {
-            name : "apple",
-            score: 8,
-            review: "great fruit"
-        }, 
-        {
-            name: "orange",
-            score: 6,
-            review: "kinda sour"
-        },
-        {
-            name: "banana",
-            score: 9,
-            review: "great stuff"
-        }
-    ], function(err, result){
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
-    })
-}
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+    name: "mango",
+    rating: 9,
+    review: "king of fruits"
+});
+
+// fruit.save();
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+    name: "john",
+    age: 79
+});
+
+person.save();
