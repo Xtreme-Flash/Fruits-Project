229aa6f2f980acaafec66c760f625ed2d718809f
Xtreme-Flash <doorway125@gmail.com>
2023-03-07 14:40:46 +0530

    added validation

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/fruitsDB',{useNewUrlParser: true});
 
 const fruitSchema = new mongoose.Schema ({
-    name: String,
-    rating: Number,
+    name: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        min:1, max:10,
+    },
     review: String
 });
 
